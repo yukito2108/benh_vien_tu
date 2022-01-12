b91bd7e9489af98d233671f99291c432be35533f
TuanNQ <tuannq@gmail.com>
2022-01-12 23:48:45 +0700

    form prescription

--- src/main/java/com/tuannq/store/model/request/PrescriptionForm.java
+++ src/main/java/com/tuannq/store/model/request/PrescriptionForm.java
@@ -4,24 +4,30 @@ import com.tuannq.store.model.annotation.Positive;
 import lombok.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class PrescriptionForm {
-    @NotNull(message = "not-null")
-    private List<@Valid Specific> medicine;
+    private List<@Valid Specific> medicine = new ArrayList<>();
 
     @Setter
     @Getter
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Specific {
+        @NotNull(message = "not-null")
+        @NotEmpty(message = "not-null")
         @Positive
         private String id;
+
+        @NotNull(message = "not-null")
+        @NotEmpty(message = "not-null")
         @Size(max = 255, message = "size-0-255")
         private String value;
     }
